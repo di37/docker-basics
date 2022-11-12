65eef16004c8f6ac8f762dc7a5bda4c0ae8bb6c0
isham993 <d.isham.993@gmail.com>
2022-11-13 00:14:26 +0400

    example of mounting volume included.

--- example2/main.py
+++ example2/main.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
@@ -13,3 +14,14 @@ async def read_root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/heros/{emp_id}")
+async def get_hero(emp_id: int):
+
+    f = open("records.json")
+    data = json.load(f)
+    for record in data["emp_details"]:
+        if record["emp_id"] == emp_id:
+            return record
+    return {"Error": "Record not found!"}
